95c6274eb3ed2c1fdb77f5a09761b95a7216f604
Octoberfest7 <91164728+Octoberfest7@users.noreply.github.com>
2021-10-31 10:36:35 -0700

    Add files via upload

---
+++ D_invoke/dll/Template.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.IO;
+using System.Security.Cryptography;
+using System.Collections.Generic;
+namespace dll
+{
+    [ComVisible(true)]
+    public class Class1
+    {
+	<IMPORTS>
+	public static void runner()
+        {
+		<MAIN>
+	}
+	<DECRYPTFUNC>
+    }
+    <CLASSES>
+}
